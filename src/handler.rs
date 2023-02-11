754532cdb3d7af1619186dfa5c27575fa9335dea
CODEVO <contact@codevoweb.com>
2023-02-11 06:23:45 +0000

    updated

--- src/handler.rs
+++ src/handler.rs
@@ -16,7 +16,7 @@ use uuid::Uuid;
 
 #[get("/healthchecker")]
 async fn health_checker_handler() -> impl Responder {
-    const MESSAGE: &str = "Implement Google and GitHub OAuth2 in Rust";
+    const MESSAGE: &str = "How to Implement Google and GitHub OAuth2 in Rust";
 
     HttpResponse::Ok().json(serde_json::json!({"status": "success", "message": MESSAGE}))
 }
